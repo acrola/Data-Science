b5490fdd953f68c6ab6538a2ed04ad67dfd39630
dor <dorbank@gmail.com>
2016-11-29 18:09:51 +0200

    first time good

--- Assignment 1 Code.R
+++ Assignment 1 Code.R
@@ -9,8 +9,8 @@
 # assignment folder for easy access. From now on do not use the full path,
 # but only the name of the file within this path.
 ################################
-setwd("C:\\Users\\Avi\\AppData\\Roaming\\SPB_16.6\\Data-Science")
-setwd("C:\\Users\\Avi\\AppData\\Roaming\\SPB_16.6\\Data-Science")
+#setwd("C:\\Users\\Avi\\AppData\\Roaming\\SPB_16.6\\Data-Science")
+setwd("C:\\Users\\dor\\Documents\\Data-Science")
 
 
 # 1.c. (3) Import the CSV file "movies.csv" into R and save it
